547f6866cce7731a9aa2017e68052e2d082cfe31
andrea <andrefp90@fullsail.edu>
2016-06-07 13:19:30 -0500

    changes

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var name=prompt("Please type in your name:");
+console.log (name);
+
+alert("Welcome"+" "+name+"! Let's play a game");
+
+var game =prompt("How much distance dose a gnu cover in their journey? \ A)3000 miles B)2500 miles C)1800 miles");
+
+if (game ==="c"){
+	alert("Congratulations!!"+" "+name+" "+"You are right the gnu travel 1800 miles in a clockwise circle");
+	}else{
+	alert("Sorry :)"+" "+name+" "+"The correct answer is gnus travel 1800 miles in a clockwise circle");
+	}
